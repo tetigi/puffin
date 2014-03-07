1b7167ce6d9bbcc7b0e724444e0dcd67bbcd0557
Luke Tomlin <ltomlin@palantir.com>
2014-03-07 13:54:22 +0000

    Trying a new method of volume generation

--- src/main/scala/examples/Generation.scala
+++ src/main/scala/examples/Generation.scala
@@ -1,11 +1,8 @@
 package com.puffin.examples
 
-import org.lwjgl.input._
-import org.lwjgl.opengl.Display
-import org.lwjgl.opengl.GL11
+import scala.collection.immutable.HashMap
 
-import scala.math._
-
+import com.puffin.utils.PuffinApp
 import com.puffin.utils.generation.VolumeGenerator
 import com.puffin.objects.Volume
 import com.puffin.Common._
@@ -28,7 +25,21 @@ object Generation {
     Context.debug = true
 
     //val volume = VolumeGenerator.genVolumeFromBitmap("resources/face.bmp")
-    val volume = VolumeGenerator.genVolumeFromBitmap("resources/thing.bmp")
+    //val volume = VolumeGenerator.genVolumeFromBitmap("resources/tree_front.bmp")
+    //val volume = VolumeGenerator.genVolumeFromBitmap("resources/face3D_front.bmp")
+    //val volume = VolumeGenerator.genVolumeFromBitmap("resources/thing.bmp")
+    //val volume = VolumeGenerator.genVolumeFromBitmaps("resources/tv_front.bmp", "resources/tv_left.bmp")
+    //val volume = VolumeGenerator.genVolumeFromBitmaps("resources/face3D_front.bmp", "resources/face3D_left.bmp")
+    //val volume = VolumeGenerator.genVolumeFromBitmaps("resources/sphere.bmp", "resources/sphere.bmp")
+    val faces = 
+      new HashMap() ++ 
+        List("front" -> "resources/sphere.bmp", 
+             "back" -> "resources/sphere.bmp",
+             "left" -> "resources/sphere.bmp", 
+             "right" -> "resources/sphere.bmp", 
+             "top" -> "resources/sphere.bmp", 
+             "bottom" -> "resources/sphere.bmp")
+    val volume = VolumeGenerator.genVolumeFromBitmaps2(faces)
     World.putThing(volume)
 
     opts.setOcclusionEnabled(true)
