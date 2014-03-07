d90b6292d6b5a9fabab4af6069c80954ab903864
Luke Tomlin <ltomlin@palantir.com>
2014-03-07 14:09:55 +0000

    New method is way better. Why didn\'t I think of this before. Also messed up the merge a little bit so fixed that.

--- src/main/scala/examples/Generation.scala
+++ src/main/scala/examples/Generation.scala
@@ -4,26 +4,11 @@ import scala.collection.immutable.HashMap
 
 import com.puffin.utils.PuffinApp
 import com.puffin.utils.generation.VolumeGenerator
-import com.puffin.objects.Volume
-import com.puffin.Common._
-import com.puffin.utils.ShaderUtils._
-import com.puffin.utils._
 import com.puffin.context.World
-import com.puffin.context.Context
-import com.puffin.character.Entity
 
-object Generation {
-  // The array containing volume data
-  val SIZE = 164
-  val WIDTH = 1024
-  val HEIGHT = 768
-
-  val opts = new RenderOptions()
-
-  def start() = {
+class Generation extends PuffinApp {
+  def setupWorld() {
     
-    Context.debug = true
-
     //val volume = VolumeGenerator.genVolumeFromBitmap("resources/face.bmp")
     //val volume = VolumeGenerator.genVolumeFromBitmap("resources/tree_front.bmp")
     //val volume = VolumeGenerator.genVolumeFromBitmap("resources/face3D_front.bmp")
@@ -42,56 +27,16 @@ object Generation {
     val volume = VolumeGenerator.genVolumeFromBitmaps2(faces)
     World.putThing(volume)
 
-    opts.setOcclusionEnabled(true)
-
-    // Setup input
-    Keyboard.enableRepeatEvents(true)
-
-    // init OpenGL
-    setupOpenGL(WIDTH, HEIGHT)
-    setupMatrices(WIDTH, HEIGHT) 
-    setupShaders("shaders/vert.glsl", "shaders/frag.glsl", "shaders/geom.glsl")
     World.entity.moveToCell(0, 10, 20)
     World.entity.lookLat(180.toRadians)
     World.entity.lookLng(20.toRadians)
-
-
-
-    var ticker = 0
-    while (! Display.isCloseRequested()) {
-      loopCycle()
-      Display.sync(60)
-      Display.update()
-      World.entity.update(1.0f/60f)
-    }
-
-    // Finish
-    destroyOpenGL()
-    println("Done!")
   }
+}
 
-  def renderCycle() = {
-    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT)
-    // Set all the matrices
-    storeMatrices()
-    // Get quads and render them
-    World.renderWorld(opts)
-  }
-
-  def logicCycle() = {
-    World.model.rotation.y += 1.0f/12f
-    Entity.controlCycle(World.entity)
-  }
-
-  def loopCycle() = {
-    // Update logic
-    logicCycle()
-    // Update rendered frame
-    renderCycle()
-  }
-
+object Generation {
   def main(args : Array[String]) = {
-    Generation.start()
+    val app = new Generation()
+    app.start()
   }
 }
 
